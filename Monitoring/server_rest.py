1079dc65fe00b7d5ef0a4c96e8d649d031c104bd
Hamza SAFRI <safrihamza.ensasafi@gmail.com>
2025-10-04 16:19:22 +0200

    UPDATE MONITOR

--- Monitoring/server_rest.py
+++ Monitoring/server_rest.py
@@ -47,8 +47,15 @@ class TerminalColor(Enum):
     YELLOW = '\033[33m'
     BLUE = '\033[34m'
     CYAN = '\033[36m'
+    MAGENTA = '\033[35m'
     WHITE = '\033[97m'
     RESET = '\033[0m'
+    BRIGHT_RED = '\033[91m'
+    BRIGHT_GREEN = '\033[92m'
+    BRIGHT_YELLOW = '\033[93m'
+    BRIGHT_BLUE = '\033[94m'
+    BRIGHT_CYAN = '\033[96m'
+    BRIGHT_MAGENTA = '\033[95m'
 
     def apply(self, text):
         return f"{self.value}{text}{TerminalColor.RESET.value}"
